b571ce67a69d909c38b6e5934ee593374d88095d
Donald Whitely <dswhitely1@gmail.com>
2020-08-02 11:37:11 -0400

    Added API

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,8 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from "redux-devtools-extension"
 import rootReducer from './reducers';
-const middleware = [thunk];
+import api from "../api";
+const middleware = [thunk.withExtraArgument(api)];
 const enhancers = applyMiddleware(...middleware);
 
 const store = createStore(rootReducer,composeWithDevTools(enhancers));
