9eb3c83c9582ce0494e21d7c11919fd011b67155
Donald Whitely <dswhitely1@gmail.com>
2020-08-02 10:43:20 -0400

    Specify app should list restaurants

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,4 @@ import {render} from 'react-dom';
 
 import App from './App';
 
-render(<StrictMode><App />></StrictMode>, document.getElementById('root'));
+render(<StrictMode><App /></StrictMode>, document.getElementById('root'));
